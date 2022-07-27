9563d7b6d6419045905fe922148acf343da2c2a9
Noble <ruzibizanoble@gmail.com>
2022-07-27 06:21:03 -0700

    Script should output: SENDER, RECEIVER & FLAGS

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,5 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+!/usr/bin/env ruby
+# Textme
+puts ARGV[0]
+       .scan(/(?<=from:|to:|flags:).*?(?=\])/)
+       .join(',')
